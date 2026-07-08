2a37b3a4fda32212886fd22f3eae203aa9ee3444
Marcelo Félix <marcelofpfelix@gmail.com>
2026-07-08 11:50:22 +0000

    test: enforce 100 lighthouse scores

--- .lighthouserc.cjs
+++ .lighthouserc.cjs
@@ -7,19 +7,21 @@ module.exports = {
         "http://127.0.0.1:4325/",
         "http://127.0.0.1:4325/posts/",
         "http://127.0.0.1:4325/search/",
-        "http://127.0.0.1:4325/posts/markdown-feature-sample/",
+        "http://127.0.0.1:4325/posts/hello-papyrus/",
       ],
       numberOfRuns: 1,
       settings: {
+        chromePath: "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
         preset: "desktop",
+        throttlingMethod: "provided",
       },
     },
     assert: {
       assertions: {
-        "categories:performance": ["error", { minScore: 0.9 }],
-        "categories:accessibility": ["error", { minScore: 0.95 }],
-        "categories:best-practices": ["error", { minScore: 0.95 }],
-        "categories:seo": ["error", { minScore: 0.95 }],
+        "categories:performance": ["error", { minScore: 1 }],
+        "categories:accessibility": ["error", { minScore: 1 }],
+        "categories:best-practices": ["error", { minScore: 1 }],
+        "categories:seo": ["error", { minScore: 1 }],
       },
     },
     upload: {
