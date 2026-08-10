f103859a61146a84887f7976aab4f5d6ebf2532e
Marcelo Félix <marcelofpfelix@gmail.com>
2026-08-10 15:27:08 +0100

    chore: inherit papyrus routes

--- src/pages/llms.txt.ts
+++ src/pages/llms.txt.ts
@@ -3,7 +3,7 @@ import { postHref, publishedPosts } from "astro-theme-papyrus/utils";
 import { site } from "../site.config";
 
 export async function GET({ site: astroSite }: { site?: URL }) {
-  const base = astroSite?.toString().replace(/\/$/, "") ?? "https://blog.marcelofelix.com";
+  const base = astroSite?.toString().replace(/\/$/, "") ?? "https://marcelofelix.com";
   const posts = publishedPosts(await getCollection("posts"));
   const lines = [
     `# ${site.title}`,
